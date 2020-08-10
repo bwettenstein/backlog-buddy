1bba2a62caf7b17f1238594328b3ed245ea58145
bwettenstein <wettenstein@gmail.com>
2020-08-10 15:51:54 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Request URL
+// https://api-v3.igdb.com
+// App name
+// backlog-buddy's App
+// Key
+// 6f648d71547350ea7a3e9591fe90438d
+// Add this as a user-key parameter to your API calls to authenticate.
